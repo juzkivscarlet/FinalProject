cdb0f10b6a9cdf977a14f20d8781de40b3a7a363
mattjuskiw <juskiwmatt@gmail.com>
2020-01-07 20:04:38 -0500

    Updated Sequelize MVC or some stuff

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const sequelize = require('sequelize');
 const path = require('path');
+const session = require('express-session');
+const validator = require('express-validator');
+
+const db = require('./models');
+const passport = require('./config/passport');
+require('dotenv');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -9,11 +16,14 @@ app.use(express.json());
 if(process.env.NODE_ENV==='production') app.use(express.static('client/build'));
 require('./routes');
 
+app.use(session({secret:'doog', saveUninitialized:false, resave:false}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get('/', (req,res) => {
 	res.sendFile(path.join(__dirname,'./client/build/index.html'));
 });
-
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbnamehere");
 
 app.listen(PORT, () => {
 	console.log(`Live on localhost:${PORT}`);
