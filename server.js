0c2ff4de8c13be8d913203f83a95ba5d673d74a1
mattjuskiw <juskiwmatt@gmail.com>
2020-01-28 19:36:20 -0500

    Changed local port

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const routes = require('./routes');
 require('dotenv');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
