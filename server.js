7c1a6640efc1fbcc077e27d3a06b6f6c27365ea0
mattjuskiw <juskiwmatt@gmail.com>
2020-01-28 19:14:28 -0500

    Fixing routing issues

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-if(process.env.NODE_ENV==='production') app.use(express.static('client/build'));
 app.use(session({secret:'dog', saveUninitialized:true, resave:true}));
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(routes);
+if(process.env.NODE_ENV==='production') app.use(express.static('client/build'));
 
 db.sequelize.sync().then(() => {
 	app.listen(PORT, () => {
